edb5730747649a3a5ecab3fb457f684fa24c28cb
Arunachalam33 <carunachalam791@gmail.com>
2025-06-13 00:38:59 +0530

    Fix: import Navigation

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { isAuthenticated } from "./utils/auth";
 
 ReactDOM.render(

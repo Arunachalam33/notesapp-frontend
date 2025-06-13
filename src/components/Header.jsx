8d46cbc6ac115b317ca701540d6d8a62fea3e759
Arunachalam33 <carunachalam791@gmail.com>
2025-06-13 16:13:45 +0530

    ADD logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import HighlightIcon from "@mui/icons-material/Highlight";
+import { AuthContext } from "../utils/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+
+   const { logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    logout(); // clears localStorage token
+    navigate("/login"); // redirect to login page
+  }
+
   return (
     <header>
       <h1>
         <HighlightIcon />
         Keeper
       </h1>
+      <button onClick={handleLogout}>Logout</button>
     </header>
   );
 }
 
 export default Header;
+
+

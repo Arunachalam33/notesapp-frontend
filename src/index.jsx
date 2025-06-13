e71ddb5e1123def06f2c86d2362670a37214e608
Arunachalam33 <carunachalam791@gmail.com>
2025-06-13 10:47:15 +0530

    Add protectedRoute

--- src/index.jsx
+++ src/index.jsx
@@ -5,16 +5,29 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { isAuthenticated } from "./utils/auth";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={isAuthenticated()?<App />:<Navigate to="/login"/>} />
-      <Route path="/login" element={isAuthenticated()?<Navigate to="/"/>:<Login />}/>
-      <Route path="/register" element={isAuthenticated()?<Navigate to="/"/>:<Register />} />
+      {/* Home route is protected */}
+      <Route path="/" element={<ProtectedRoute><App /></ProtectedRoute>} />
+      
+      {/* Login & Register should be open for unauthenticated users */}
+      <Route path="/login" element={
+        isAuthenticated() ? <Navigate to="/" /> : <Login />
+      } />
+      
+      <Route path="/register" element={
+        isAuthenticated() ? <Navigate to="/" /> : <Register />
+      } />
+
+      {/* Catch-all route */}
+      <Route path="*" element={<Navigate to={isAuthenticated() ? "/" : "/login"} />} />
     </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
 
 
